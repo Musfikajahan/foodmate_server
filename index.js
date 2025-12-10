7eb63d65c345c801d32c342984c89f9c5ce4da69
MusfikaJahan <musfikajahanmithun7117@gmail.com>
2025-12-11 00:49:19 +0600

    mongodb connection

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+// MongoDB Connection
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true }
+});
+
+
+async function run() {
+    try {
+        const db = client.db("foodchefDB");
+        const usersCollection = db.collection("users");
+        const mealsCollection = db.collection("meals");
+        const ordersCollection = db.collection("orders");
+        const paymentsCollection = db.collection("payments");
+
+
