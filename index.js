e65a410d5810f94e6ab6f82e16c551dbed1efff1
MusfikaJahan <musfikajahanmithun7117@gmail.com>
2025-12-22 10:57:11 +0600

    updated the cors error

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5000',
+      'https://foodmate-2d2d9.web.app',
+      'https://foodshare-gamma.vercel.app'
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+  app.use(express.json());
 
 // MongoDB Connection Setup
 const uri = process.env.MONGO_URI;
